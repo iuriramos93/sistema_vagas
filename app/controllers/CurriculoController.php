2389c3e42295e38034b954faf2fd494163d34db4
iuriramos93 <35300523+iuriramos93@users.noreply.github.com>
2020-05-28 13:36:51 -0300

    Rota do cadastro curriculo
    Alterado o direcionamento da rota após cadastro de Currículo

--- app/controllers/CurriculoController.php
+++ app/controllers/CurriculoController.php
@@ -46,7 +46,7 @@ if(cadastrar_curriculo($novo_nome,$id_user)&& $upload){
      $_SESSION['alerta']['tipo'] = 'alert-success';
      $_SESSION['alerta']['mensagem'] = "Currículo Cadastrado com Sucesso";
      //redireciona o usuário
-     header('Location:/admin/curriculo/cadastrar');
+     header('Location:/admin/curriculo');
      //aborta a execução
      exit;
 }
